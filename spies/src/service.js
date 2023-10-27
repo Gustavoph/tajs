be92323a0eb7ee043a62306adc377b2c81f3b316
Gustavo <gusta.coliveira07@gmail.com>
2023-10-27 11:49:34 -0300

    add spies class

---
+++ spies/src/service.js
@@ -0,0 +1,43 @@
+import crypto from 'node:crypto'
+import fs from 'node:fs/promises'
+import fsSync from 'node:fs'
+
+export class Service {
+  #filename
+
+  constructor({ filename }) {
+    this.#filename = filename
+  }
+
+  #hashPassword(password) {
+    return crypto.createHash('sha256')
+      .update(password)
+      .digest('hex')
+  }
+
+  create({ username, password }) {
+    const data = JSON.stringify({
+      username, 
+      password: this.#hashPassword(password),
+      createdAt: new Date().toISOString()
+    }).concat('\n')
+
+    return fs.appendFile(this.#filename, data)
+  }
+
+  async read() {
+    const exists = fsSync.existsSync(this.#filename)
+    if (!exists) return []
+
+    const lines = (await fs.readFile(this.#filename, 'utf-8'))
+      .split('\n')
+      .filter(line => !!line)
+
+      if(!lines.length) return []
+
+      return lines
+        .map(line => JSON.parse(line))
+        .map(({ password, ...rest }) =>({...rest}) )
+
+  }
+}
